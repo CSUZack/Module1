8a4b6f79fd34afb678885a8135bf808a47794b95
Zack <59975166+Xerronn@users.noreply.github.com>
2024-07-21 18:04:46 -0400

    Final commit

---
+++ Part2.py
@@ -0,0 +1,19 @@
+#recursive function to ask for a valid integer and to ask for a retry on any value error
+def InputValidInt(prompt, retries=5):
+    if retries <= 0:
+        print("Too many failures in user input, aborting.\n")
+        exit()
+    try:
+        return int(input(prompt))
+    except ValueError:
+        print("Something was wrong with that input, please try again.\n")
+        retries -=1
+        return InputValidInt(prompt, retries)
+    
+num1 = InputValidInt("Please type your first number and then press enter.\n")
+num2 = InputValidInt("Please type your second number and then press enter.\n")
+
+print("\nResults:\n")
+print(f"{num1} x {num2} = {num1*num2}\n")
+print(f"{num1} / {num2} = {num1/num2}\n")
+print("Thanks for using my program.\n")
